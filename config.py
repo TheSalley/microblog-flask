2319b023190cd7023bba1fad20a26d7ff288519a
theSalley <1919532973@qq.com>
2023-01-14 10:56:19 +0800

    改配置

--- config.py
+++ config.py
@@ -4,5 +4,10 @@
 # @File: config.py
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a secret string'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
